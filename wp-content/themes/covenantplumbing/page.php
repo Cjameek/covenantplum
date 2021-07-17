361ebb1f2b8729a8db1c3d8b410c9f213832e8b7
Cjameek <cody@mandr-group.com>
2021-07-17 12:20:33 -0400

    Remove container around title

--- wp-content/themes/covenantplumbing/page.php
+++ wp-content/themes/covenantplumbing/page.php
@@ -2,9 +2,7 @@
 <main id="primary-wrap" class="primary-content" role="main">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class('page post-holder'); ?>>
-            <div class="container">
-                <?php get_template_part( 'template-parts/title' ); ?>
-            </div>
+            <?php get_template_part( 'template-parts/title' ); ?>
             <?php get_template_part( 'template-parts/advanced-layout' ); ?>
         </article>
     <?php endwhile; endif; ?>
