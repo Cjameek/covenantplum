42167bfff8908247285a5ef2cd288a1116fe1b36
Cjameek <cody@mandr-group.com>
2021-07-19 18:26:41 -0400

    Add primary-content__modules wrap

--- wp-content/themes/covenantplumbing/front-page.php
+++ wp-content/themes/covenantplumbing/front-page.php
@@ -8,9 +8,11 @@ get_header(); ?>
 <main id="primary-wrap" class="primary-content" role="main">
 	<?php get_template_part( 'template-parts/homepage/hero' ); ?>
 	
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php get_template_part( 'template-parts/advanced-layout' ); ?>
-	<?php endwhile; endif; ?>
+	<div class="primary-content__modules">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php get_template_part( 'template-parts/advanced-layout' ); ?>
+		<?php endwhile; endif; ?>
+	</div>
 </main>
 
 <?php get_footer(); ?>
