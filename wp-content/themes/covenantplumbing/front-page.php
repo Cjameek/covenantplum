fe742c8397cc1539d83628d4ec378d5db0c00aec
Cjameek <cody@mandr-group.com>
2021-07-19 17:02:12 -0400

    Update hero template part name

--- wp-content/themes/covenantplumbing/front-page.php
+++ wp-content/themes/covenantplumbing/front-page.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 
 <main id="primary-wrap" class="primary-content" role="main">
-	<?php get_template_part( 'template-parts/slider' ); ?>
+	<?php get_template_part( 'template-parts/homepage/hero' ); ?>
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<?php get_template_part( 'template-parts/advanced-layout' ); ?>
